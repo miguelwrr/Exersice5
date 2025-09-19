5e4da2a1b92630865925b826c41d0ecdad4e22b0
mw.rodriguez <mw.rodriguez@106-24.info2>
2025-09-19 12:47:10 +0200

    Exersice 5 1.0

---
+++ src/exersice5/Exersice5.java
@@ -0,0 +1,12 @@
+package exersice5;
+
+public class Exersice5 {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Miguel Rodríguez.");
+		System.out.println("Address: C\\Cueva de Menga, 3, 41020, Seville, Spain.");
+		System.out.println("Telephone number: +34696977634");
+
+	}
+
+}
